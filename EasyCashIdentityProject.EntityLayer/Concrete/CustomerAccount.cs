52cb70181b7fa26c175ee403ded4d7879e8b1951
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-03-23 13:05:39 +0300

    Müşterinin Hareketler Tablosu İle İlişkisi

--- EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccount.cs
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccount.cs
@@ -15,5 +15,7 @@ namespace EasyCashIdentityProject.EntityLayer.Concrete
         public string BankBranch { get; set; }
         public int AppUserID { get; set; }
         public AppUser AppUser { get; set; }
+        public List<CustomerAccountProcess> CustomerSender { get; set; }
+        public List<CustomerAccountProcess> CustomerReceiver { get; set; }
     }
 }
